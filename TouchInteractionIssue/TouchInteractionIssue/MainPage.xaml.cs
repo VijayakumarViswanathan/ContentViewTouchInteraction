cc786ce1309fcc808b9122111e046538dce25df3
Vijayakumar <vijayakumar.v@syncfusion.com>
2022-11-22 16:14:13 +0530

    Committing the sample

---
+++ TouchInteractionIssue/TouchInteractionIssue/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+namespace TouchInteractionIssue;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		DisplayAlert("Alert!", "From first view button event", "ok");
+	}
+}
+public class FirstView : ContentView
+{
+
+}
+
+public class SecondView : ContentView
+{
+
+}
